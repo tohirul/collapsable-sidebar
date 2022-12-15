c1c2c35fe71368678b01fc01a9da2ca46c6e8744
Tohirul Islam <tohirul.islam164@gmail.com>
2022-12-15 22:57:34 +0600

    Toggle Active Class on Click Event

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ const menuIconButton = document.querySelector("[data-menu-icon-btn]");
 const sidebar = document.querySelector("[data-sidebar]");
 
 menuIconButton.addEventListener("click", () => {
-    sidebar.classList.toggle("open")
+	sidebar.classList.toggle("open");
 });
+
+const listItems = document.querySelectorAll(".sidebar-list-item");
+console.log(listItems);
+
+for (const item of listItems) {
+	// console.log(item);
+	item.addEventListener("click", () => {
+		listItems.forEach((item) => item.classList.remove("active"));
+		item.classList.add("active");
+	});
+}
